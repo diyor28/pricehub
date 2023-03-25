c868a025f5111287a81f6233421b2e62f47fad35
Sardor Absamatov <absamatovs7@gmail.com>
2023-03-25 13:53:59 +0500

    Categories render

--- pricehub/views.py
+++ pricehub/views.py
@@ -100,3 +100,27 @@ class Login(View):
             return render(request, "login.html", context={"error": "password is incorrect"})
 
         return redirect('/')
+
+
+from django.views import View
+from django.shortcuts import render
+import requests
+
+class CategoriesView(View):
+    def category_box(self, categories):
+        items = []
+        for category in categories:
+            if category["children"]:
+                items += self.category_box(category["children"])
+            else:
+                items.append(category)
+        return items
+
+    def get_categories(self):
+        response = requests.get("https://api.umarket.uz/api/main/root-categories?eco=false", headers={"Authorization": "Basic YjJjLWZyb250OmNsaWVudFNlY3JldA==", "Accept-Language": "ru-RU"})
+        categories = response.json()["payload"]
+        return self.category_box(categories)
+
+    def get(self, request):
+        categories = self.get_categories()
+        return render(request, 'categories.html', {'categories': categories})
