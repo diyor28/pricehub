f3f8f71bb538e5ab9e6c7f0ac85c5e7aa0f293b9
shukurovozodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2023-03-25 18:05:54 +0500

    some changes

--- pricehub/views.py
+++ pricehub/views.py
@@ -119,7 +119,7 @@ class CategoriesView(View):
     def get_categories(self):
         response = requests.get("https://api.umarket.uz/api/main/root-categories?eco=false", headers={"Authorization": "Basic YjJjLWZyb250OmNsaWVudFNlY3JldA==", "Accept-Language": "ru-RU"})
         categories = response.json()["payload"]
-        return self.category_box(categories)
+        return self.category_box(categories[:100])
 
     def get(self, request):
         categories = self.get_categories()
