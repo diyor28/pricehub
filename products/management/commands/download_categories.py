ec2a5c87eb1ab62a446c1dcc249118bc311be639
Sardor <absamatovs7@gmail.com>
2023-04-01 17:58:46 +0500

    Categories donwload function v2.0

--- products/management/commands/download_categories.py
+++ products/management/commands/download_categories.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from products.models import CategoriesModel
 import requests
 
-
 class CategoriesView:
     def category_box(self, categories):
         items = []
@@ -18,10 +17,21 @@ class CategoriesView:
                                 headers={"Authorization": "Basic YjJjLWZyb250OmNsaWVudFNlY3JldA==",
                                          "Accept-Language": "ru-RU"})
         categories = response.json()["payload"]
-        print(response.json()["payload"])
         return self.category_box(categories[:100])
 
-
+    def variables(categoryId: int, offset: int, limit: int):
+        return {
+            "queryInput": {
+                "categoryId": str(categoryId),
+                "showAdultContent": "TRUE",
+                "filters": [],
+                "sort": "BY_ORDERS_NUMBER_DESC",
+                "pagination": {
+                    "offset": offset,
+                    "limit": limit
+                }
+            }
+        }
 class Command(BaseCommand):
     help = 'Downloads categories from API'
 
@@ -29,6 +39,9 @@ class Command(BaseCommand):
         categories_view = CategoriesView()
         categories = categories_view.get_categories()
         for category in categories:
-            save_data = CategoriesModel()
+            save_data = CategoriesModel(title = category["title"],
+                                        remote_id = category["id"],
+                                        source = "uzum"
+                                        )
             save_data.save()
         self.stdout.write(self.style.SUCCESS('Categories downloaded successfully.'))
