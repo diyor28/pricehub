9d67c6d55b8b3ddde0e504a98708b1f9080d8806
Sardor <absamatovs7@gmail.com>
2023-04-01 17:05:06 +0500

    categories download function

--- pricehub/views.py
+++ pricehub/views.py
@@ -119,3 +119,4 @@ class CategoriesView(View):
     def get(self, request):
         categories = self.get_categories()
         return render(request, 'categories.html', {'categories': categories})
+
