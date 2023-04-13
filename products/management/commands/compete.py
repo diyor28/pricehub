7ffc375db40bf7c595a52dfbb8a7c8eed4732571
Diyor Khaydarov <diyor28july@gmail.com>
2023-04-13 19:07:06 +0500

    complete solution

--- products/management/commands/compete.py
+++ products/management/commands/compete.py
@@ -1,6 +1,11 @@
+from queue import Queue
+from threading import Thread
+
+import requests
 from django.core.management import BaseCommand
 
 from pricehub.products import timeit
+from pricehub.views import headers
 
 urls = [
     "https://api.umarket.uz/api/v2/product/389704",
@@ -38,10 +43,36 @@ urls = [
 ]
 
 
+def do_task(task_q, res_q):
+    while not task_q.empty():
+        url = task_q.get(block=True)
+        resp = requests.get(url, headers=headers)
+        res_q.put(resp.json())
+
+
 class Command(BaseCommand):
     help = 'Downloads categories from API'
 
     @timeit
     def handle(self, *args, **options):
-        assert len(results) == len(urls)
-        print('Congrats! Drinks on me')
+        task_q = Queue(maxsize=1000)
+        res_q = Queue(maxsize=1000)
+        fabrics = []
+        sklad = []
+
+        for url in urls:
+            task_q.put(url)
+
+        for i in range(10):
+            th = Thread(target=do_task, args=(task_q, res_q))
+            th.start()
+            fabrics.append(th)
+
+        while len(sklad) < len(urls):
+            sklad.append(res_q.get(block=True))
+
+        for th in fabrics:
+            th.join()
+
+        # your code goes here
+        assert len(sklad) == len(urls)
