93ef7c318ff2502253972eb31dc667da7809b2e5
Diyor Khaydarov <diyor28july@gmail.com>
2023-05-13 20:56:03 +0500

    push

--- iterators.py
+++ iterators.py
@@ -6,8 +6,7 @@ def my_fancy_iterator(start, end):
 
 
 if __name__ == '__main__':
+    numbers = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
     it = my_fancy_iterator(0, 100)
     for i in range(10):
-        next(it)
-
-    assert processed == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+        assert processed == numbers[:i + 1]
