28bc03d572db234d648c0e3ba75d5420c48631e7
Diyor Khaydarov <diyor28july@gmail.com>
2023-06-07 00:32:21 +0500

    vector search

--- products/management/commands/vector_search.py
+++ products/management/commands/vector_search.py
@@ -11,6 +11,7 @@ from django.core.management import BaseCommand
 from numba import njit, prange
 
 from products.models import ProductModel
+import tensorflow_hub as hub
 
 
 @njit(parallel=True, fastmath=True)
@@ -35,35 +36,42 @@ class Command(BaseCommand):
     help = 'Vector search'
 
     @staticmethod
+    def _build_custom_model():
+        mnet = tf.keras.applications.MobileNetV3Large(input_shape=(224, 224, 3))
+        logits = tf.keras.layers.Conv2D(1137, kernel_size=1, padding="same", name="Logits")(mnet.layers[-4].output)
+        flat = tf.keras.layers.Flatten(name="flatten")(logits)
+        m = tf.keras.Model(inputs=mnet.inputs, outputs=[flat])
+        m.compile()
+        return m
+
+    @staticmethod
     def _build_model():
-        model = tf.keras.applications.ResNet50V2(pooling='avg', include_top=False, input_shape=(224, 224, 3))
-        model.load_weights(os.path.join(settings.BASE_DIR, 'encoder.h5'))
-        model.compile(jit_compile=True)
-        model.summary()
-        return model
+        m = tf.keras.Sequential([
+            hub.KerasLayer("https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2", trainable=False)
+        ])
+        m.build((224, 224, 3))
+        return m
 
     @staticmethod
     def _load_image(path: str):
         image = tf.io.read_file(path)
-        image = tf.image.decode_image(image, expand_animations=False, channels=3, dtype=tf.float32)
-        image = tf.image.resize(image, [224, 224])
-        return tf.expand_dims(tf.keras.applications.resnet_v2.preprocess_input(image), axis=0)
+        image = tf.image.decode_image(image, expand_animations=False, channels=3)
+        image = tf.image.resize(image, (224, 224))
+        return tf.expand_dims(image, axis=0)
 
     def add_arguments(self, parser: argparse.ArgumentParser):
         parser.add_argument("--path", type=str)
 
     def handle(self, *args, path: str, **options):
         # preheat function
-        l2_distance(np.random.random((32, 1280)).astype(np.float32), np.random.random((1280,)).astype(np.float32))
-        data = np.load(os.path.join(settings.BASE_DIR, 'index.npz'))
+        l2_distance(np.random.random((32, 2048)).astype(np.float32), np.random.random((2048,)).astype(np.float32))
+        data = np.load(os.path.join(settings.BASE_DIR, 'data/index.npz'))
         index, product_ids = data['vectors'].astype(np.float32), data['ids']
         model = self._build_model()
-        image = self._load_image(path)
-        vector = model.predict(image, verbose=0)
+        vector = model.predict(self._load_image(path), verbose=0)
         scores = l2_distance(index, vector[0])
         n_closest = np.argsort(scores)[:5]
         closest_product_ids = product_ids[n_closest]
-        print(np.min(scores), np.max(scores))
         products = ProductModel.objects.filter(id__in=list(closest_product_ids))
         for p in products:
             print(p.pk, p.title, p.url)
